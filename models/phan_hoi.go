53546d13aeac9f700efb686131b0b256a3b96144
Võ Ngân Khanh <khanhlovejijung@gmail.com>
2025-09-03 11:53:01 +0700

    update tag json

--- models/phan_hoi.go
+++ models/phan_hoi.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type PhanHoi struct {
-	ID          uint `gorm:"primaryKey;autoIncrement"`
-	KhaoSatID   uint
-	KhaoSat     KhaoSat `gorm:"foreignKey:KhaoSatID;constraint:OnDelete:CASCADE"`
-	NguoiDungID *uint
-	NguoiDung   *NguoiDung  `gorm:"foreignKey:NguoiDungID"`
-	NgayGui     time.Time   `gorm:"autoCreateTime"`
-	CauTraLois  []CauTraLoi `gorm:"foreignKey:PhanHoiID"`
+	ID          uint        `gorm:"primaryKey;autoIncrement" json:"id"`
+	KhaoSatID   uint        `json:"khao_sat_id"`
+	KhaoSat     KhaoSat     `gorm:"foreignKey:KhaoSatID;constraint:OnDelete:CASCADE" json:"khao_sat"`
+	NguoiDungID *uint       `json:"nguoi_dung_id"`
+	NguoiDung   *NguoiDung  `gorm:"foreignKey:NguoiDungID" json:"nguoi_dung"`
+	NgayGui     time.Time   `gorm:"autoCreateTime" json:"ngay_gui"`
+	CauTraLois  []CauTraLoi `gorm:"foreignKey:PhanHoiID" json:"cau_tra_loi"`
 }
