6dfdaec05fb78ee7c30cc86f25e99a96cc7c3ece
NguyenDauToan <143267702+NguyenDauToan@users.noreply.github.com>
2025-09-22 16:43:39 +0700

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173","https://nguyendautoan.github.io"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
